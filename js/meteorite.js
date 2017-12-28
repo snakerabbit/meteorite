53e9a5ab31e7fc4741b004179882007667fd8524
snakerabbit <alison.s.cheng@gmail.com>
2017-12-28 00:33:59 -0600

    refined meteorite data

--- js/meteorite.js
+++ js/meteorite.js
@@ -1,24 +1,24 @@
-import { DATA } from './dat.js';
-import Datamap from 'datamaps';
-console.log(DATA);
-console.log("name", DATA[0].name);
-console.log("geolocation", DATA[0].geolocation);
-console.log("mass", DATA[0].mass);
+// import { DATA } from './dat.js';
+// import Datamap from 'datamaps';
+// console.log(DATA);
+// console.log("name", DATA[0].name);
+// console.log("geolocation", DATA[0].geolocation);
+// console.log("mass", DATA[0].mass);
+//
+// var map = new Datamap({
+//   element: document.getElementById('container'),
+//   fills:{
+//     defaultFill: 'blue'
+//   }
+// });
 
-var map = new Datamap({
-  element: document.getElementById('container'),
-  fills:{
-    defaultFill: 'blue'
-  }
-});
 
-
-map.bubbles(DATA, {
-    popupTemplate: function (geo, data) {
-            return ['<div class="hoverinfo">' +  data.name,
-            '<br/>Payload: ' +  data.yield + ' kilotons',
-            '<br/>Country: ' +  data.country + '',
-            '<br/>Date: ' +  data.date + '',
-            '</div>'].join('');
-    }
-});
+// map.bubbles(DATA, {
+//     popupTemplate: function (geo, data) {
+//             return ['<div class="hoverinfo">' +  data.name,
+//             '<br/>Payload: ' +  data.yield + ' kilotons',
+//             '<br/>Country: ' +  data.country + '',
+//             '<br/>Date: ' +  data.date + '',
+//             '</div>'].join('');
+//     }
+// });
