ff05ff77370d4ffd148028a456e48c9c9d629c01
snakerabbit <alison.s.cheng@gmail.com>
2018-01-08 19:31:40 -0800

    integrated mapbox api

--- bundle.js
+++ bundle.js
@@ -67,30 +67,49 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-// import { DATA } from './dat.js';
-// import Datamap from 'datamaps';
-// console.log(DATA);
-// console.log("name", DATA[0].name);
-// console.log("geolocation", DATA[0].geolocation);
-// console.log("mass", DATA[0].mass);
-//
-// var map = new Datamap({
-//   element: document.getElementById('container'),
-//   fills:{
-//     defaultFill: 'blue'
-//   }
-// });
+var mapImage;
+var centerLat = 0;
+var centerLong = 0;
 
+var lat = 44.9778;
+var long = -93.2650;
+var zoom = 1;
 
-// map.bubbles(DATA, {
-//     popupTemplate: function (geo, data) {
-//             return ['<div class="hoverinfo">' +  data.name,
-//             '<br/>Payload: ' +  data.yield + ' kilotons',
-//             '<br/>Country: ' +  data.country + '',
-//             '<br/>Date: ' +  data.date + '',
-//             '</div>'].join('');
-//     }
-// });
+function preload() {
+	mapImage = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0,0/1024x512?access_token=pk.eyJ1IjoiYWxpc29uY2hlbmciLCJhIjoiY2piem0yb2FqMDA5bzMzcWh4Y3o5NjRjaSJ9.0WsEgFTZ8XPAK3qp_lgZZg');
+}
+
+function mercatorX(longitude) {
+	var longinRadians = longitude * Math.PI / 180;
+	var a = 256 / Math.PI * Math.pow(2, zoom);
+	var b = longinRadians + Math.PI;
+	return a * b;
+	console.log(a * b);
+}
+
+function mercatorY(latitude) {
+	var latinRadians = latitude * Math.PI / 180;
+	var a = 256 / Math.PI * Math.pow(2, zoom);
+	var b = Math.tan(Math.PI / 4 + latinRadians / 2);
+	var c = Math.PI - Math.log(b);
+	console.log(a * c);
+	return a * c;
+}
+function setup() {
+	createCanvas(1024, 512);
+	translate(width / 2, height / 2);
+	imageMode(CENTER);
+	image(mapImage, 0, 0);
+	var cx = mercatorX(centerLong);
+	var cy = mercatorY(centerLat);
+	var x = mercatorX(long) - cx;
+	var y = mercatorY(lat) - cy;
+	console.log(x);
+	console.log(y);
+
+	fill(255, 0, 255, 200);
+	ellipse(x, y, 20, 20);
+}
 
 /***/ })
 /******/ ]);
